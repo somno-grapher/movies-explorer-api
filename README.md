13bb591862c92b11f55592a5a5bdc6a9cd44a604
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-07-08 10:01:23 +0300

    feat: copy mesto backend, make some tweaks

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# movies-explorer-api
+[![Статус тестов](../../actions/workflows/tests.yml/badge.svg)](../../actions/workflows/tests.yml)
+
+# react-mesto-api-full
+
+Репозиторий для приложения проекта `Mesto`, включающий фронтенд и бэкенд части приложения со следующими возможностями: авторизации и регистрации пользователей, операции с карточками и пользователями. Бэкенд расположен в директории `backend/`, а фронтенд - в `frontend/`. 
+  
+Адрес репозитория: https://github.com/somno-grapher/react-mesto-api-full-gha.git
+
+## Ссылки на проект
+
+IP 158.160.6.37
+
+Frontend https://mestofrontend.nomoredomains.rocks
+
+Backend https://mestobackend.nomoredomains.rocks
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const userRouter = require('./users');
+const cardRouter = require('./cards');
+const usersController = require('../controllers/users');
+const auth = require('../middlewares/auth');
+const { validateUserBody } = require('../middlewares/validate');
+const NotFoundError = require('../errors/NotFoundError');
+
+// TODO: exclude after review
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+router.use('/signup', validateUserBody, usersController.createUser);
+router.use('/signin', validateUserBody, usersController.login);
+
+router.use(auth);
+router.use('/users', userRouter);
+router.use('/cards', cardRouter);
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Маршрут не найден'));
+});
+
+module.exports = router;
